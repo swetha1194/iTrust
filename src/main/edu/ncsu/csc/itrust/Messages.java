545b78fa9f10b29284c7ca173d3bbe0b3789de52
Sumer Shende <sam.sumer@gmail.com>
2017-03-14 14:43:35 -0400

    Update Messages.java

--- src/main/edu/ncsu/csc/itrust/Messages.java
+++ src/main/edu/ncsu/csc/itrust/Messages.java
@@ -6,7 +6,7 @@ import java.util.ResourceBundle;
 /**
  */
 public class Messages {
-	private static final String BUNDLE_NAME = "edu.ncsu.csc.itrust.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "edu.ncsu.csc.itrust"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, Localization.instance().getCurrentLocale());
 
